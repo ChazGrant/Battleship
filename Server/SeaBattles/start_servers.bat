c4abb93f2157874501ba9de7bdbc4d5453ec154f
ChazGrant <ilya20012209@yandex.ru>
2024-04-05 13:37:39 +0600

    Added ability to  specify which server to start

--- Server/SeaBattles/start_servers.bat
+++ Server/SeaBattles/start_servers.bat
@@ -1,2 +1,7 @@
-start python manage.py runserver 127.0.0.1:8000
-start uvicorn SeaBattles.asgi:application --host 127.0.0.1 --port 8080
+IF "%~1"=="-drf" ( 
+    start python manage.py runserver 127.0.0.1:8000
+) ELSE IF "%~1"=="-wss" (
+    start uvicorn SeaBattles.asgi:application --host 127.0.0.1 --port 8080
+) ELSE (
+    IF "%~1"=="" start python manage.py runserver 127.0.0.1:8000 && start uvicorn SeaBattles.asgi:application --host 127.0.0.1 --port 8080
+)
